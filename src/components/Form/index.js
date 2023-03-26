2cc51e7e3362af81c7d5d978e7143116cb5f7051
yraffic <yraffic@gmail.com>
2023-03-25 23:09:26 -0300

    new input

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,69 +1,69 @@
-import { Button } from '../Button'
-import './style.css'
-import { useForm } from 'react-hook-form';
-import {yupResolver} from '@hookform/resolvers/yup'
-import * as yup from 'yup'
-import {toCanvas, toDataURL} from 'qrcode'
+import { yupResolver } from '@hookform/resolvers/yup';
+import { toDataURL } from 'qrcode';
 import { useState } from 'react';
-import QRCode from 'react-qr-code';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { Button } from '../Button';
+import './style.css';
 
-const validateForms =  yup.object().shape({
+const validateForms = yup.object().shape({
     name: yup.string().required("required name"),
-    linkedIn:yup.string().required('required linkedIn'),
-    gitHub: yup.string().required('required gitHub')  
+    linkedIn: yup.string().required('required linkedIn'),
+    gitHub: yup.string().required('required gitHub'),
+    description: yup.string().required('required description')
 })
 
 export const Forms = () => {
-    const {register, handleSubmit, formState: {errors}} = useForm({
+    const { register, handleSubmit, formState: { errors } } = useForm({
         resolver: yupResolver(validateForms)
     })
     const [qrcodeLink, setQrCodeLink] = useState('')
-    
-    const handleGenerate = (link_url)=>{
-        toDataURL(link_url,{
+
+    const handleGenerate = (link_url) => {
+        toDataURL(link_url, {
             width: 600,
             margin: 3
-        }, function(err, url){
-            if(err){
-            return    console.log(err)
+        }, function (err, url) {
+            if (err) {
+                return console.log(err)
             }
-           return setQrCodeLink(url)
+            return setQrCodeLink(url)
 
         })
     }
-    
-    const download = async (user)=>{
+
+    const download = async (user) => {
         handleGenerate(`http://localhost:3000/${user}`);
-        
+
         const img = new Image();
         img.src = qrcodeLink;
         img.width = '300px'
         img.height = '300px'
-        
+
         img.onload = () => {
             const link = document.createElement('a');
             link.href = qrcodeLink;
             link.download = `${user}-qrcode.png`;
-            
+
             link.appendChild(img);
-            
+
             document.body.appendChild(link);
-            
+
             link.click();
-            
+
             document.body.removeChild(link);
-          }
+        }
     }
 
     const form = async (data) => {
         const user = data.name
-        
+
         // gera o QR Code
-       download(user)
-      }
-      
-      
-   
+        download(user)
+    }
+
+
+
 
     return (
         <form onSubmit={handleSubmit(form)}>
@@ -95,8 +95,8 @@ export const Forms = () => {
                     {...register('linkedIn')}
                     className='input-component'
                 />
-                <span 
-                className='error-message'
+                <span
+                    className='error-message'
                 >
                     {errors.linkedIn?.message}
                 </span>
@@ -114,17 +114,36 @@ export const Forms = () => {
                     {...register('gitHub')}
                     className='input-component'
                 />
-                <span 
-                className='error-message'
+                <span
+                    className='error-message'
                 >
                     {errors.gitHub?.message}
+                </span>
+            </div>
+            <div className='conteiner-input-component' >
+                <div className="label-component">
+                    <label htmlFor='Description' >
+                        Description
+                    </label>
+                </div>
+                <input
+                    type='text'
+                    id='Description'
+                    name='description'
+                    {...register('description')}
+                    className='input-component'
+                />
+                <span
+                    className='error-message'
+                >
+                    {errors.description?.message}
                 </span>
             </div>
             <Button
                 type='submit'
                 width='30%'
             >
-               Genereate Image
+                Genereate Image
             </Button>
         </form>
     )
